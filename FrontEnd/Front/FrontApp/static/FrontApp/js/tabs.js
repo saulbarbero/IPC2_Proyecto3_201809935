9ade31ec859aa5211ab62a0e5a2616465d550e0e
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2022-04-16 22:17:33 -0600

    Front version1 terminado

---
+++ FrontEnd/Front/FrontApp/static/FrontApp/js/tabs.js
@@ -0,0 +1,28 @@
+$('ul.tabs li a:first').addClass('active');
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
+
+	$('ul.tabs li a').click(function(){
+		$('ul.tabs li a').removeClass('active');
+		$(this).addClass('active');
+		$('.secciones article').hide();
+
+		var activeTab = $(this).attr('href');
+		$(activeTab).show();
+		return false;
+	});
+
+
+
+/* Filtro tabs*/
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
